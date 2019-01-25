47ae9f3d3c5b431edb2c144a0e903f45e3916e9c
satyazmx <thakursatya6@gmail.com>
2019-01-25 16:14:39 +0530

    structure ready

---
+++ ShoppingCart/src/app/shared/ingredient.ts
@@ -0,0 +1,15 @@
+export class Ingredient{
+
+    constructor(public name: string,public amount: number){}
+   /*  typeScript provide shortcut to get rid of below code
+   public name: string;
+    public amount: number;
+
+    constructor(name: string,amount: number){
+        this.name=name,
+        this.amount=amount
+
+    }*/
+
+
+}
--- ShoppingCart/src/app/shopping-list/shopping-list.component.ts
+++ ShoppingCart/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient'
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients : Ingredient[]=[
+     new Ingredient('Palak Paneer',12),
+     new Ingredient('Kellogs',20)
+  ];
 
   constructor() { }
 
