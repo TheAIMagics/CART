2fb40dee5862e209f994ab5a2f0305d6cc646de4
satyazmx <thakursatya6@gmail.com>
2019-01-25 14:47:05 +0530

    templating bootstrap

--- ShoppingCart/src/app/recipes/recipes-list/recipes-list.component.ts
+++ ShoppingCart/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+
+  recipes :Recipe[] = [
+
+    new Recipe('Pasta','Test Recipe',
+    'https://sandbox-uploads.imgix.net/u/1548402925-1a76d09a91c147e7516d5f19ddf5526f?w=600'),
+    new Recipe('PanCake','Test Recipe',
+    'https://sandbox-uploads.imgix.net/u/1548404361-99faca462541d45079a23c26f322e7e2?w=600')
+  ];
 
   constructor() { }
 
